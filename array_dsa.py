57d406491c7906e8c3b72a83f0e40edc9af202de
Priti Rathod <preeti005rathod@gmail.com>
2026-01-01 19:01:46 +0530

    array expenses qn

---
+++ array_dsa.py
@@ -0,0 +1,24 @@
+# 1. Let us say your expense for every month are listed below,
+# 	1. January -  2200
+#  	2. February - 2350
+#     3. March - 2600
+#     4. April - 2130
+#     5. May - 2190
+#
+# Create a list to store these monthly expenses and using that find out,
+#
+# 1. In Feb, how many dollars you spent extra compare to January?
+# 2. Find out your total expense in first quarter (first three months) of the year.
+# 3. Find out if you spent exactly 2000 dollars in any month
+# 4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
+# 5. You returned an item that you bought in a month of April and
+# got a refund of 200$. Make a correction to your monthly expense list
+# based on this
+
+exp = [2200, 2350, 2600, 2130, 2190]
+print("In feb you spent extra $", exp[1]-exp[0])
+print("total expense in first quarter is", exp[0]+exp[1]+exp[2])
+print("did you spend exactly 2000 dollars in any month?", 2000 in exp)
+exp.append(1980)
+exp[3] -= 200
+print("updated expense list:", exp)
