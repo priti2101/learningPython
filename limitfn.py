d4eb8349479a43176833e1cd369cbff658aedb01
Priti Rathod <preeti005rathod@gmail.com>
2026-02-07 05:49:30 +0530

    limit

---
+++ limitfn.py
@@ -0,0 +1,15 @@
+def limit(arr, min_lim = None, max_lim = None):
+    if len(arr)==0:
+        return arr
+    if min_lim is None:
+        min_lim == min(arr)
+    if max_lim is None:
+        max_lim == max(arr)
+    
+    return list(filter(lambda x: (min_lim<=x<=max_lim), arr))
+
+print(limit([1,2,3,4,5], 2, 4))
+
+
+
+
